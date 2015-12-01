dbcd287170e6b44231b14f2fab0da7105060780b
Eugenio <geniokk@gmail.com>
2015-12-02 00:26:54 +0100

    refactoring with underscore

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 var PORT = process.env.PORT || 3000;
 // var todos = [{
 // 	id: 1,
@@ -34,26 +35,41 @@ app.get('/todos', function (req, res) {
 
 app.get('/todos/:id', function (req,res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo;
+	var matchedTodo = _.findWhere(todos,{id: todoId});
 
-	for (var i = todos.length - 1; i >= 0; i--) {
-		if (todos[i].id === todoId){
-			matchedTodo = todos[i];
-		}
-	};
+	//  ---- without underscore ------
+	// var matchedTodo;
+
+	// for (var i = todos.length - 1; i >= 0; i--) {
+	// 	if (todos[i].id === todoId){
+	// 		matchedTodo = todos[i];
+	// 	}
+	// };
+	//  ------ end underscore -----
 	
 	if (matchedTodo) {
 		res.json(matchedTodo);
 	} else {
-		res.status(404).send('The id ' + req.params.id + ' has not been found');
+		res.status(404).send('The id ' + 
+			req.params.id + ' has not been found');
 	};
 	// res.send('Asking for todos with id of ' + req.params.id);
 });
 
 app.post('/todos', function (req, res) {
-	var body = req.body;
+	var body = _.pick(req.body, 'description', 'completed');
 
+
+	if (!_.isBoolean(body.completed) 
+		|| !_.isString(body.description) 
+		|| body.description.trim().length === 0 ) {
+
+		return res.status(400).send();
+	}
 	// ---- course solution ----
+
+	body.description = body.description.trim();
+
 	body.id = todoNextId ++;
 	todos.push(body);
 
@@ -69,7 +85,7 @@ app.post('/todos', function (req, res) {
 
 	// todoNextId ++;
 
-	console.log('description: ' + body.description);
+	// console.log('description: ' + body.description);
 
 	res.json(body);
 });
